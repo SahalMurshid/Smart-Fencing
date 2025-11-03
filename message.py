9ebe7a0fe8cb1b4b2d88e6c208b1bd0c404da8f7
Sahal Murshid <114126173+SahalMurshid@users.noreply.github.com>
2025-11-03 20:40:13 +0530

    Anonymize sensitive data in message.py
    Replaced sensitive account SID and auth token with placeholders and anonymized recipient number.

--- message.py
+++ message.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
-account_sid = 'ACc4b1fc1af33d85a0b87d95d5fe7ee4a0'
-auth_token = 'dd35b00d7280a26b1deace4bf006d4d2'
+account_sid = '******************************'
+auth_token = '********************************'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
   from_='+15076836292',
   body='Hey nigga good to meet you there is a elephant near the fence dont go near it ',
-  to='+918606496666'
+  to='+1***********'
 )
 
 print(message.sid)
